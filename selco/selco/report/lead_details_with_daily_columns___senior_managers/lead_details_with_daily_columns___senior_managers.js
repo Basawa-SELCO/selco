0841818fffd654ffc594729e400d1a457b7d65b0
Basawaraj <basawaraj@selco-india.com>
2017-06-08 08:35:08 +0530

    Filter in lead reports

--- selco/selco/report/lead_details_with_daily_columns___senior_managers/lead_details_with_daily_columns___senior_managers.js
+++ selco/selco/report/lead_details_with_daily_columns___senior_managers/lead_details_with_daily_columns___senior_managers.js
@@ -3,6 +3,21 @@
 
 frappe.query_reports["Lead Details with Daily Columns - Senior Managers"] = {
 	"filters": [
+		{
+	"fieldname": "fiscal_year",
+	"label": __("Fiscal Year"),
+	"fieldtype": "Link",
+	"options": "Fiscal Year",
+	default: "2017-2018"
+	},
+	{
+		"fieldname":"month_number",
+		"label": __("Month"),
+		"fieldtype": "Select",
+		"options": "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec",
+		"default": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov",
+			"Dec"][frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth()],
+	}
 
 	]
 }
