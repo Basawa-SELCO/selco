2968c0c628e3eb7742496b22997ee99e2a3b4056
Basawaraj <basawaraj@selco-india.com>
2017-08-11 11:46:54 +0530

    Added Financing Institution and Financing Institution Branch Doctypes

---
+++ selco/selco/doctype/financing_institution_branch/financing_institution_branch.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, Selco and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Financing Institution Branch', {
+	refresh: function(frm) {
+
+	}
+});
