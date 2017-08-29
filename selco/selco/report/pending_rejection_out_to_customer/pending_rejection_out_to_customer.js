e0f0b9c1a004ddecad7d99171a86d70aba85cccc
Basawaraj <basawaraj@selco-india.com>
2017-08-29 15:45:57 +0530

    Added -Pending rejection Out To Customer - Query Report

---
+++ selco/selco/report/pending_rejection_out_to_customer/pending_rejection_out_to_customer.js
@@ -0,0 +1,12 @@
+frappe.query_reports["Pending Rejection Out To Customer"] = {
+
+"filters": [
+    {
+        "fieldname":"branch",
+        "label": "Branch",
+        "fieldtype": "Link",
+        "options": "Branch",
+        "reqd": 1
+    }
+]
+}
