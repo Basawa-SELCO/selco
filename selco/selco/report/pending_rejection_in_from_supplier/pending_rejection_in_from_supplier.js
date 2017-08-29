0ee6ed7f1570da263675b42a124a26fdca49e843
Basawaraj <basawaraj@selco-india.com>
2017-08-29 17:10:44 +0530

    Added Pending Rejection in from suppplier - query report

---
+++ selco/selco/report/pending_rejection_in_from_supplier/pending_rejection_in_from_supplier.js
@@ -0,0 +1,12 @@
+frappe.query_reports["Pending Rejection In From Supplier"] = {
+
+"filters": [
+    {
+        "fieldname":"branch",
+        "label": "Branch",
+        "fieldtype": "Link",
+        "options": "Branch",
+        "reqd": 1
+    }
+]
+}
