61c4168ab2e44252480b040c01f8cbe35e21b313
Basawaraj <basawaraj@selco-india.com>
2017-11-25 13:30:22 +0530

    changes related to basic rate in stock entry

--- selco/selco/doctype/selco_customizations/selco_customizations.py
+++ selco/selco/doctype/selco_customizations/selco_customizations.py
@@ -167,6 +167,10 @@ def selco_stock_entry_updates(doc,method):
     selco_selco_warehouse = frappe.db.get_value("Branch",doc.branch,"selco_warehouse")
     selco_repair_warehouse = frappe.db.get_value("Branch",doc.branch,"repair_warehouse")
 
+    for d in doc.get('items'):
+        d.basic_rate = 0
+
+
     if doc.purpose=="Material Transfer":
         if doc.inward_or_outward=="Inward" and doc.type_of_stock_entry == "GRN":
             doc.naming_series = frappe.db.get_value("Branch",doc.branch,"receipt_note_naming_series")
@@ -176,7 +180,6 @@ def selco_stock_entry_updates(doc,method):
                     d.from_warehouse = "SELCO GIT - SELCO"
                     d.to_warehouse = selco_selco_warehouse
                     d.reference_rej_in_or_rej_quantity = doc.suppliers_ref
-                    d.basic_rate = 0
             else:
                 for d in doc.get('items'):
                     d.s_warehouse = "SELCO GIT Repair - SELCO"
@@ -184,7 +187,6 @@ def selco_stock_entry_updates(doc,method):
                     d.cost_center = selco_cost_center
                     d.is_sample_item = 1
                     d.reference_rej_in_or_rej_quantity = doc.suppliers_ref
-                    d.basic_rate = 0
         elif doc.inward_or_outward=="Inward" and doc.type_of_stock_entry == "Demo - Material Return":
             for d in doc.get('items'):
                 d.cost_center = selco_cost_center
@@ -199,7 +201,6 @@ def selco_stock_entry_updates(doc,method):
                     d.s_warehouse = selco_selco_warehouse
                     d.t_warehouse = "SELCO GIT - SELCO"
                     d.cost_center = selco_cost_center
-                    d.basic_rate = 0
             else:
                 doc.from_warehouse = selco_repair_warehouse
                 doc.to_warehouse = "SELCO GIT Repair - SELCO"
@@ -208,7 +209,6 @@ def selco_stock_entry_updates(doc,method):
                     d.t_warehouse = "SELCO GIT Repair - SELCO"
                     d.cost_center = selco_cost_center
                     d.is_sample_item = 1
-                    d.basic_rate = 0
         elif doc.inward_or_outward=="Outward" and doc.type_of_stock_entry== "Demo - Material Issue":
             doc.naming_series = frappe.db.get_value("Branch",doc.branch,"delivery_note_naming_series")
             for d in doc.get('items'):
@@ -238,7 +238,6 @@ def selco_stock_entry_updates(doc,method):
             d.cost_center = selco_cost_center
             d.s_warehouse = selco_selco_warehouse
             d.t_warehouse = selco_selco_warehouse
-            d.basic_rate = 0
     if doc.type_of_stock_entry == "Outward DC":
         doc.recipient_email_id = frappe.db.get_value("Branch",doc.being_dispatched_to,"branch_email_id")
 
